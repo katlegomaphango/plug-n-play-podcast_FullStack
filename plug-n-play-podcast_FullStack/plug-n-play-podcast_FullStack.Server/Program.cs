937adfd7d45d8b0b03c65508c43ebb655992f7c5
katlegomaphango <mphangokatlego08@gmail.com>
2024-08-23 22:03:34 +0200

    services

--- plug-n-play-podcast_FullStack/plug-n-play-podcast_FullStack.Server/Program.cs
+++ plug-n-play-podcast_FullStack/plug-n-play-podcast_FullStack.Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using plug_n_play_podcast_FullStack.Server.Data;
+using plug_n_play_podcast_FullStack.Server.Models;
+
 namespace plug_n_play_podcast_FullStack.Server
 {
     public class Program
@@ -7,10 +11,39 @@ namespace plug_n_play_podcast_FullStack.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddIdentityCore<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                //user setttings
+                options.User.AllowedUserNameCharacters =
+                    "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-._@+";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+
             var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
@@ -20,7 +53,7 @@ namespace plug_n_play_podcast_FullStack.Server
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.MapIdentityApi<User>();
 
             app.MapControllers();
 
