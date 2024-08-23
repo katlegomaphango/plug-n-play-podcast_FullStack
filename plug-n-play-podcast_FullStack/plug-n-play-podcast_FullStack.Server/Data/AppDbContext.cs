4e5622a04d4e7d77fdd5fec6a385f86753de0a08
katlegomaphango <mphangokatlego08@gmail.com>
2024-08-23 21:58:46 +0200

    appDbContext

---
+++ plug-n-play-podcast_FullStack/plug-n-play-podcast_FullStack.Server/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using plug_n_play_podcast_FullStack.Server.Models;
+
+namespace plug_n_play_podcast_FullStack.Server.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Preview> Previews { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Show> Shows { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configuring entities' relationships and properties here
+
+            // Preview-Show Relationship
+            builder.Entity<Preview>().HasOne(p => p.Show).WithOne().HasForeignKey<Preview>(p => p.Id);
+
+            // Preview-Genre Many-to-Many Relationship
+            builder.Entity<Preview>().HasMany(p => p.Genres).WithMany(g => g.Previews);
+
+            // Show-Genre Many-to-Many Relationship
+            builder.Entity<Show>().HasMany(s => s.Genres).WithMany(g => g.Shows);
+
+            // Show-Season One-to-Many Relationship
+            builder.Entity<Show>().HasMany(s => s.Seasons).WithOne();
+
+            // Season-Episode One-to-Many Relationship
+            builder.Entity<Season>().HasMany(s => s.Episodes).WithOne();
+        }
+    }
+}
---
+++ plug-n-play-podcast_FullStack/plug-n-play-podcast_FullStack.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace plug_n_play_podcast_FullStack.Server.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
