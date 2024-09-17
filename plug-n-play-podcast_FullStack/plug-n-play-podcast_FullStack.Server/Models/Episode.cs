633655cb92c47382331cec50a430ebeee2954b54
katlegomaphango <mphangokatlego08@gmail.com>
2024-09-17 11:49:54 +0200

    backend initial

--- plug-n-play-podcast_FullStack/plug-n-play-podcast_FullStack.Server/Models/Episode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace plug_n_play_podcast_FullStack.Server.Models
-{
-    public class Episode
-    {
-        public int Id { get; set; }
-        public string File { get; set; } = string.Empty;
-        public string Title { get; set; } = string.Empty;
-    }
-}
