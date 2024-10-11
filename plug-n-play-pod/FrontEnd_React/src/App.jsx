8c07d043dbeb594a61e070d3307ff8652852f225
Katlego Maphango <mphangokatlego08@gmail.com>
2024-10-11 20:03:49 +0200

    sign up page

--- plug-n-play-pod/FrontEnd_React/src/App.jsx
+++ plug-n-play-pod/FrontEnd_React/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/NavBar/Navbar'
+import SignUp from './pages/AdminPage'
 import Login from './pages/LoginPage'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </>
   )
 }
